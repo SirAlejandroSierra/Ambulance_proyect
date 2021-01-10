56b15d381e1f4953f0146ee6231b4ebfd7980daa
SirAlejandroSierra <alexsitorr@gmail.com>
2021-01-10 19:42:20 +0100

    Cerrado forzado ambulancia

--- Telemedicine_ambulance_project_2/src/Client/UserController.java
+++ Telemedicine_ambulance_project_2/src/Client/UserController.java
@@ -7,11 +7,12 @@ package Client;
 
 import Patient.Users;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.URL;
+import java.security.NoSuchAlgorithmException;
+import java.security.spec.InvalidKeySpecException;
 import java.util.ArrayList;
 import java.util.ResourceBundle;
 import java.util.logging.Level;
@@ -33,48 +34,52 @@ import javafx.stage.Stage;
  * @author AdriCortellucci
  */
 public class UserController implements Initializable {
-    
-    @FXML private TextField userName; 
-    @FXML private TextField password;
-    @FXML private Label error;
-    
+
+    @FXML
+    private TextField userName;
+    @FXML
+    private TextField password;
+    @FXML
+    private Label error;
+
     private Socket socket;
     private ObjectOutputStream toServer;
     private Stage window;
     private ObjectInputStream fromServer;
-    private ArrayList<Users>  users =new ArrayList<Users>();
-    
-    public void initData (Socket socket, Stage stage, ObjectInputStream oi, ObjectOutputStream oo) throws IOException{
-        this.socket=socket;
-        this.window=stage;
-        this.fromServer= oi;
-        this.toServer= oo;
-        
+    private ArrayList<Users> users = new ArrayList<Users>();
+
+    public void initData(Socket socket, Stage stage, ObjectInputStream oi, ObjectOutputStream oo) throws IOException {
+        this.socket = socket;
+        this.window = stage;
+        this.fromServer = oi;
+        this.toServer = oo;
+
         window.setOnCloseRequest((event) -> {
             releaseResources();
         });
     }
-    
-    public boolean checkUser(Users u){
-        for(Users user: users){
-            if(u.equals(user)){
+
+    public boolean checkUser(Users u, String pintroduced) throws NoSuchAlgorithmException, InvalidKeySpecException {
+        for (Users user : users) {
+            if (u.authenticate(pintroduced, user.getPassword(), user.getsalt())) {
                 return true;
             }
         }
         return false;
     }
-    
+
     @FXML
-    public void next(ActionEvent event)throws IOException {
-        Users u= new Users(userName.getText(), password.getText());
+    public void next(ActionEvent event) throws IOException, NoSuchAlgorithmException, InvalidKeySpecException {
+        String pintroduced = password.getText();
+        Users u = new Users(userName.getText(), password.getText());
         try {
-            users= (ArrayList<Users>) fromServer.readObject();
+            users = (ArrayList<Users>) fromServer.readObject();
         } catch (ClassNotFoundException ex) {
             Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
         }
-        if(!checkUser(u)){
+        if (!checkUser(u, pintroduced)) {
             error.setText("Wrong user or password");
-        }else{
+        } else {
             System.out.println("Good user");
 
             FXMLLoader loader = new FXMLLoader();
@@ -86,20 +91,20 @@ public class UserController implements Initializable {
 
             AmbulanceWindowController controller = loader.getController();
             Stage window = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        
-            try{
+
+            try {
                 controller.initData(socket, window, fromServer, toServer);
-            }catch(Exception e){
+            } catch (Exception e) {
                 e.printStackTrace();
             }
-            
+
             window.setScene(scene);
             window.show();
         }
     }
-    
+
     public void backToConnection(ActionEvent event) throws IOException {
-        
+
         releaseResources();
         FXMLLoader loader = new FXMLLoader();
         loader.setLocation(getClass().getResource("ConnectToServer.fxml"));
@@ -110,8 +115,8 @@ public class UserController implements Initializable {
         window.setScene(scene);
         window.show();
     }
-    
-    private void releaseResources()  {
+
+    private void releaseResources() {
         try {
             toServer.writeObject("logout");
         } catch (IOException ex) {
@@ -121,26 +126,26 @@ public class UserController implements Initializable {
             toServer.close();
         } catch (IOException ex) {
             Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
-        } 
+        }
         try {
             fromServer.close();
         } catch (IOException ex) {
             Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
-        } 
+        }
         try {
             socket.close();
             System.out.println("socket closed");
         } catch (IOException ex) {
             Logger.getLogger(UserController.class.getName()).log(Level.SEVERE, null, ex);
-        } 
+        }
     }
-    
+
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
 }
